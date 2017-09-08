aa569fb06e0f66fa213f8cadf7b05b260ade53c3
josephverbeck <joseph.verbeck@nih.gov>
2017-09-08 14:25:32 -0400

    Reverting so intervention cancergov sort doesnt affect disease search

--- api/search/searcher.js
+++ api/search/searcher.js
@@ -785,24 +785,13 @@ class Searcher {
     let bool = innerAgg[path + "_filtered"]["filter"]["query"]["bool"];
     let inInterventionsOrDiseases = q["agg_field"] === "_aggregates.interventions" || q["agg_field"] === "_aggregates.diseases";
     if (q["sort"] || q["order"]) {
-        let validSort = q["sort"] === "count" || q["sort"] === "name";
-        let validOrder = q["order"] === "asc" || q["order"] === "desc";
+        let validSort   = q["sort"] === "count" || q["sort"] === "name";
+        let validOrder  = q["order"] === "asc" || q["order"] === "desc";
         if (validSort && validOrder) {
             let sortKey = "_" + q["sort"].replace("name", "term");
             groupAgg[path]["terms"]["order"][sortKey] = q["order"];
-        } else if (q["sort"] === "cancergov" && inInterventionsOrDiseases) {
-            if(q["category"] === undefined){
-              //Would love to just throw an error here....-jv
-              CT_API_ERROR = new Error("Category must be defined when using sort cancergov");
-            }
-            let sortKey = "_" + "count";
-            if (q["category"] === "agent") {
-                groupAgg[path]["terms"]["order"][sortKey] = "desc";
-            } else {
-                groupAgg[path]["terms"]["order"][sortKey] = "asc";
-            }
         } else if (!(inInterventionsOrDiseases && q["sort"] === "cancergov")) {
-            CT_API_ERROR = new Error("Parameters missing or incorrect. Sort can only be by one of the following [name, count, cancergov] and order can only be descending (desc) or ascending (asc).");
+            CT_API_ERROR = new Error("Parameters missing or incorrect. Sort can only be by (name) or (count) and order can only be descending (desc) or ascending (asc).");
         }
     }
 
@@ -835,16 +824,18 @@ class Searcher {
           "size": 0
         }
       };
-
-      //Moved to the top
-      // if (q["sort"] === "cancergov") { //why?...this shouldn't be a thing. -jv
-      //     if(q["category"] === undefined){
-      //       CT_API_ERROR = new Error("Category must be defined when using sort cancergov");
-      //     }
-      //   delete groupAgg[path]["terms"]["order"];
-      //   groupAgg       [path]["aggs"][path + ".type"    ]["terms"]["order"] = {"_term": "asc"};
-      //   groupAgg       [path]["aggs"][path + ".category"]["terms"]["order"] = {"_term": "asc"};
-      // }
+      if (q["sort"] === "cancergov"){
+          if(q["category"] === undefined){
+              //Would love to just throw an error here....-jv
+              CT_API_ERROR = new Error("Category must be defined when using sort cancergov");
+          }
+          let sortKey = "_" + "count";
+          if (q["category"] === "agent") {
+              groupAgg[path]["terms"]["order"][sortKey] = "desc";
+          } else {
+              groupAgg[path]["terms"]["order"][sortKey] = "asc";
+          }
+      }
 
       this._filterAggByField(path, bool["must"][0]["bool"]["should"], q["type"],     "type._fulltext");
       this._filterAggByField(path, bool["must"][0]["bool"]["should"], q["category"], "category._fulltext");
